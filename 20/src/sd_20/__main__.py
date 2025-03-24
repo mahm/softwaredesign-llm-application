1230d3435cc585b651c2373395b89fffddbc565b
Masahiro Nishimi <mah@edged-edge.jp>
2025-03-24 16:56:44 +0900

    fix: thread_id

--- 20/src/sd_20/__main__.py
+++ 20/src/sd_20/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import uuid
 
 from src.sd_20.agent import graph
 
@@ -9,7 +10,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # エージェントの実行
+    config = {'configurable': {'thread_id': str(uuid.uuid4())}}
     user_input = {"messages": [("user", args.query)]}
-    for state in graph.stream(user_input, stream_mode="values"):
+    for state in graph.stream(user_input, stream_mode="values", config=config):
         msg = state["messages"][-1]
         msg.pretty_print()
